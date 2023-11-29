79cdec36447fb890b8d27ed47931c4ad05d21c69
IgorLms <sigor97@mail.ru>
2023-11-29 10:31:16 +0300

    Добавил метод добавления параметров камеры в JSON файл

--- window_app/app.py
+++ window_app/app.py
@@ -30,3 +30,30 @@ class App(ApplicationDesign):
 
         data_json.update({name: path, })
         json.dump(data_json, open('data/data.json', 'w'), sort_keys=True, indent=2, ensure_ascii=False)
+
+    def _add_camera_json(self) -> None:
+        """Добавление камеры в JSON файл"""
+
+        # Валидация данных
+        if not 1 <= len(self.name_camera.text()) <= 19:
+            self._create_error("Длина названия камеры должна быть от 1 до 19 символов")
+        elif len(self.rtsp.text()) == 0:
+            self._create_error("Укажите RTSP поток камеры")
+        else:
+            # Прочитаем JSON файл
+            data_json = self._get_json()
+            if self.name_camera.text() in data_json.keys():
+                self._create_error("Название камеры уже существует")
+            elif len(data_json) > 10:
+                self._create_error("Можно добавить не более 10 камер")
+                # Отчистить поле RTSP
+                self.rtsp.clear()
+            else:
+                # Добавление камеры в JSON файл
+                self.__update_json(data_json, self.name_camera.text(), self.rtsp.text())
+                # Создание новых кнопок
+                self._create_button(self.name_camera.text(), self.rtsp.text())
+                # Отчистить поле RTSP
+                self.rtsp.clear()
+            # Отчистить поле названия камеры
+            self.name_camera.clear()
