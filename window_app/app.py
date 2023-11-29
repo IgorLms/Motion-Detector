13346f7578c1ee07e07da9006ad650fc867d7630
IgorLms <sigor97@mail.ru>
2023-11-29 10:38:13 +0300

    Добавил методы для просмотра, остановки и переключения видеопотока с камеры в приложении

--- window_app/app.py
+++ window_app/app.py
@@ -1,6 +1,14 @@
+from PyQt5.QtCore import pyqtSlot, Qt
+from PyQt5.QtGui import QPixmap
+from PyQt5 import QtGui
+
+import numpy as np
 import json
+import cv2
 
 from window_app.designer import ApplicationDesign
+from video.motion_detector import SubtractionFrame
+from video.video import VideoCaptureRTSP
 
 
 class App(ApplicationDesign):
@@ -57,3 +65,90 @@ class App(ApplicationDesign):
                 self.rtsp.clear()
             # Отчистить поле названия камеры
             self.name_camera.clear()
+
+    def __stop_video(self) -> None:
+        """Остановить видео просмотр"""
+
+        # Остановить просмотр видео, если он существует
+        if self.__thread:
+            self.__thread.stop()
+        # Удалить последний кадр с виджета image_label
+        self.image_label.clear()
+
+    def __start_video(self) -> None:
+        """Начать видео просмотр"""
+
+        # Подключение к сигналу просмотра видео
+        self.__thread.change_pixmap_signal.connect(self.__update_image)
+        # запустить просмотр видео
+        self.__thread.start()
+
+    def __run_video(self, class_video):
+        """Запуск видео"""
+
+        # Если путь для запуска видео существует
+        if self.__path or type(self.__path) == int and self.__path == 0:
+            self.__stop_video()
+            # Класс для получения видео
+            try:
+                # То запустить просмотр видео
+                self.__thread = class_video
+                self.__start_video()
+            except Exception as e:
+                # вывести ошибку, если при запуске видео возникли ошибки
+                self._create_error(str(e))
+                self.__stop_video()
+
+    def __run_open_video(self) -> None:
+        """Запуск видео основного потока"""
+
+        if self.__path or type(self.__path) == int and self.__path == 0:
+            self.__run_video(VideoCaptureRTSP(self.__path))
+        else:
+            self._create_error('Укажите камеру')
+
+    def __run_open_filter_video(self) -> None:
+        """Запуск видео с фильтром"""
+
+        if self.__path or type(self.__path) == int and self.__path == 0:
+            self.__run_video(SubtractionFrame(self.__path))
+        else:
+            self._create_error('Укажите камеру')
+
+    @pyqtSlot(np.ndarray)
+    def __update_image(self, frame: QPixmap) -> None:
+        """Метод обновления изображения на видео"""
+
+        # По сигналу остановить просмотр видео
+        self.__thread.signal_stop_video.connect(self.__stop_video)
+        # Преобразование изображения из OpenCV в QPixmap
+        frame = self.__convert_cv_qt(frame)
+        # Поместить изображение в виджет
+        self.image_label.setPixmap(frame)
+
+    def __convert_cv_qt(self, frame: cv2.typing.MatLike) -> QPixmap:
+        """Преобразование изображения из OpenCV в QPixmap"""
+
+        # Преобразование изображения в RGB
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        # Получение размеров изображения
+        h, w, ch = frame.shape
+        # Конвертирование в QPixmap
+        convert_to_qt_format = QtGui.QImage(frame.data, w, h, ch * w, QtGui.QImage.Format_RGB888)
+        # Копия изображения масштабируемая под виджет
+        frame_to_widget = convert_to_qt_format.scaled(self.image_label.size(), Qt.IgnoreAspectRatio)
+        return QPixmap.fromImage(frame_to_widget)
+
+    def _button_open_video(self, path: str) -> None:
+        """Открыть видео по кнопке"""
+
+        # Меняем RTSP путь до видео
+        try:
+            # Проверка, что путь к камере это число
+            self.__path = int(path)
+        except ValueError:
+            # Иначе сохранить, как строка
+            self.__path = path
+
+        # Запускаем видео
+        self.__run_open_video()
